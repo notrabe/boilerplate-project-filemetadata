7645c6cae5a560a1b793129736d5923c842493de
Evan Barton <notrabe1011@gmail.com>
2021-01-10 03:10:22 -0500

    finished project

--- server.js
+++ server.js
@@ -13,8 +13,17 @@ app.get('/', function (req, res) {
 
 
 
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+const multer = require('multer')
+
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+  const responseObject = {}
+  responseObject['name'] = req.file.originalname
+  responseObject['type'] = req.file.mimetype
+  responseObject['size'] = req.file.size
+  res.json(responseObject)
+})
